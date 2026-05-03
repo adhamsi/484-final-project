253bb1dd1d8c0bca5caa9d16bfbd26c0397429d4
Adham Ibrahim <adham.ibrahim@princeton.edu>
2026-05-03 00:31:24 -0400

    run on 3 and 5

--- scripts/game24/bfs.sh
+++ scripts/game24/bfs.sh
@@ -1,7 +1,7 @@
 python run.py \
     --task game24 \
     --task_start_index 900 \
-    --task_end_index 1000 \
+    --task_end_index 950 \
     --method_generate propose \
     --method_evaluate value \
     --method_select greedy \
